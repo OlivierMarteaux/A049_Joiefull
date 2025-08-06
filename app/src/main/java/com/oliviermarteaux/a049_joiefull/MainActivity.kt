2724c6522f45552593fe70e546acce90a16e530f
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-08-06 16:13:19 +0200

    00_20250806_OM: initial commit

---
+++ app/src/main/java/com/oliviermarteaux/a049_joiefull/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.oliviermarteaux.a049_joiefull
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.oliviermarteaux.a049_joiefull.ui.theme.A049_JoiefullTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            A049_JoiefullTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    A049_JoiefullTheme {
+        Greeting("Android")
+    }
+}
