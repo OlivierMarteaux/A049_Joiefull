8f209c8209f60718a204feb37140c550419aee26
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-02-23 23:04:33 +0100

    13_20260223_OM: Add GooglePay button feature to ItemScreen (#12)
    * migrated to new gradle toolchain daemon
    
    * updated readme
    
    * upgrade AGP 9.0.0 > 9.0.1
    
    * removed all commented lines from app build.gradle.kts
    
    * add GooglePay dependencies
    * create CheckoutViewModel, Constants, PaymentUtils files
    * config Payment Activity
    * add GPay Button in ItemScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,15 +30,8 @@ android {
             )
         }
     }
-    // Replaced by JVM toolchain setting
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//    kotlinOptions {
-//        jvmTarget = "11"
-//    }
-    // Add JVM tooolchain to define global java version
+
+    // Add JVM toolchain to define global java version
     kotlin { jvmToolchain(17) }
 
     buildFeatures {
@@ -81,11 +74,12 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation) // Serialization support
     implementation(libs.ktor.serialization.kotlinx.json) // Kotlinx JSON
 
-    /** Retrofit for web API */
-//    implementation(libs.retrofit2.kotlinx.serialization.converter)
-//    implementation(libs.retrofit)
-//    implementation(libs.okhttp)
-//    implementation(libs.kotlinx.serialization.json)
+    /** GooglePay */
+    implementation(libs.play.services.wallet)
+    implementation(libs.compose.pay.button)
+
+    /** for tasks.await */
+    implementation(libs.kotlinx.coroutines.play.services)
 
     /** Unit tests */
     testImplementation(libs.okhttp3.mockwebserver) // MockWebServer for API testing
@@ -97,71 +91,6 @@ dependencies {
     /** debug conf to allow preview in AndroidStudio */
     testImplementation(libs.ktor.client.mock)// For MockEngine + respond
     debugImplementation(libs.ui.tooling)
-
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-//    implementation(libs.androidx.material3)
-//
-//    implementation(libs.material3)//material3 for custom ripple effect
-//    implementation(libs.androidx.activity.ktx)// activity dependency for Google ImagePicker
-//    implementation(libs.google.accompanist.systemuicontroller)// Google accompanist for SystemUiController
-//    implementation(libs.material)// google material for icons
-//    implementation(libs.androidx.navigation.compose)// Navigation
-//
-    /** Coil for image viewing */
-//    implementation(libs.coil.compose)
-//    implementation(libs.coil.network.okhttp)
-//    implementation(libs.coil.video)
-//
-    /** room */
-//    implementation(libs.androidx.room.runtime)
-//    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
-//    ksp(libs.androidx.room.compiler)
-//    // If this project only uses Java source, use the Java annotationProcessor
-//    // No additional plugins are necessary
-//    // annotationProcessor(libs.androidx.room.compiler)
-//    // optional - Kotlin Extensions and Coroutines support for Room
-//    implementation(libs.androidx.room.ktx)
-//
-    /** Retrofit for currency conversion API */
-//    implementation(libs.retrofit2.kotlinx.serialization.converter)
-//    implementation(libs.retrofit)
-//    implementation(libs.okhttp)
-//    implementation(libs.kotlinx.serialization.json)
-//
-//    testImplementation(libs.junit)
-//    testImplementation(libs.mockk) // kotlin mocking framework
-//    testImplementation(libs.kotlinx.coroutines.test) // coroutine test (runTest)
-//    testImplementation(libs.okhttp3.mockwebserver)// MockWebServer for API testing
-//    testImplementation(libs.mockito.kotlin)// Mockito
-//
-//    androidTestImplementation(libs.turbine)// Flow test
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.ui.test.junit4)
-//
-//    debugImplementation(libs.androidx.ui.tooling)
-//    debugImplementation(libs.androidx.ui.test.manifest)
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-//    implementation(libs.androidx.material3)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.ui.test.junit4)
-//    debugImplementation(libs.androidx.ui.tooling)
-//    debugImplementation(libs.androidx.ui.test.manifest)
 }
 
 /**
--- app/src/main/java/com/oliviermarteaux/a049_joiefull/MainActivity.kt
+++ app/src/main/java/com/oliviermarteaux/a049_joiefull/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.oliviermarteaux.a049_joiefull
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -13,12 +15,38 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.wallet.contract.TaskResultContracts
+import com.oliviermarteaux.a049_joiefull.ui.screens.item.CheckoutViewModel
+import com.oliviermarteaux.a049_joiefull.ui.screens.item.PaymentUiState
 import com.oliviermarteaux.a049_joiefull.ui.theme.A049_JoiefullTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+
+
+    private val paymentDataLauncher = registerForActivityResult(TaskResultContracts.GetPaymentDataResult()) { taskResult ->
+        when (taskResult.status.statusCode) {
+            CommonStatusCodes.SUCCESS -> {
+                taskResult.result!!.let {
+                    Log.i("Google Pay result:", it.toJson())
+                    model.setPaymentData(it)
+                }
+            }
+            //CommonStatusCodes.CANCELED -> The user canceled
+            //AutoResolveHelper.RESULT_ERROR -> The API returned an error (it.status: Status)
+            //CommonStatusCodes.INTERNAL_ERROR -> Handle other unexpected errors
+        }
+    }
+
+    private val model: CheckoutViewModel by viewModels()
+
+
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         // Install splash screen
@@ -29,6 +57,7 @@ class MainActivity : ComponentActivity() {
             A049_JoiefullTheme {
                 // info: layout config for Adaptive layout
                 val layoutDirection = LocalLayoutDirection.current
+                val payState: PaymentUiState by model.paymentUiState.collectAsStateWithLifecycle()
                 Surface(
                     modifier = Modifier.padding(
                         start = WindowInsets.safeDrawing.asPaddingValues()
@@ -38,9 +67,19 @@ class MainActivity : ComponentActivity() {
                     )
                 ) {
                     val windowSize = calculateWindowSizeClass(this)
-                    Joiefull(windowSize = windowSize.widthSizeClass)
+                    Joiefull(
+                        windowSize = windowSize.widthSizeClass,
+                        payUiState = payState,
+                        onGooglePayButtonClick = this::requestPayment,
+                    )
                 }
             }
         }
     }
+
+    private fun requestPayment() {
+        val task = model.getLoadPaymentDataTask(priceCents = 1000L)
+        task.addOnCompleteListener(paymentDataLauncher::launch)
+    }
+
 }
